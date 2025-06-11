5c989d82a797584a7f4580f54b6fc8cde06593d9
giovanniadinolfi <g.adinolfi39@studenti.unisa.it>
2025-06-11 22:35:41 +0200

    schermata non più ingrandibile e un po' di css

--- src/wordageddon/Wordageddon.java
+++ src/wordageddon/Wordageddon.java
@@ -16,19 +16,17 @@ import wordageddon.model.RuoloUtente;
 import wordageddon.model.Utente;
 import wordageddon.service.UtenteService;
 
-
 public class Wordageddon extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Resources/fxml/login.fxml"));
         Scene scene = new Scene(root, 900, 600);
-        
         scene.getStylesheets().add(getClass().getResource("/wordageddon/Resources/css/login.css").toExternalForm());
         stage.setTitle("Wordageddon");
-        
         stage.getIcons().add(new Image(getClass().getResourceAsStream("Resources/WordageddonG3.jpeg")));
         stage.setScene(scene);
+        stage.setResizable(false); // <-- FINESTRA NON RIDIMENSIONABILE
         stage.show();
     }
 
@@ -42,15 +40,15 @@ public class Wordageddon extends Application {
             Logger.getLogger(Wordageddon.class.getName()).log(Level.SEVERE, null, ex);
         }
         launch(args);
-        
     }
+
     // Da chiamare una sola volta all'avvio, al login o all'apertura dell'app principale
     public static void ensureAdminUserExists() throws Exception {
         UtenteDAO dao = new UtenteDAOSQL();
         UtenteService utenteservice = new UtenteService(dao);
         Utente admin = dao.cercaPerUsername("admin");
-        if (admin == null) {        
-            utenteservice.creaAdmin("admin","admin");
+        if (admin == null) {
+            utenteservice.creaAdmin("admin", "admin");
         }
     }
 }
